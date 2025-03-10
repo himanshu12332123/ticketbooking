6a8d929a1c10f13ce3206a8db3a51c39196e230e
himanshu12332123 <105910418+himanshu12332123@users.noreply.github.com>
2025-03-10 13:02:01 +0530

    added services

--- app/src/main/java/ticket/booking/entities/User.java
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -2,7 +2,7 @@ package ticket.booking.entities;
 
 import java.util.List;
 
-public class user {
+public class User {
     private String name;
     private String password;
     private String hashPassword;
